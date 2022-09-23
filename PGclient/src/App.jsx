b83483ad13f692389f1c366a4698c571de747cad
samyakkhatua <samyakkhatua@duck.com>
2022-09-24 01:03:09 +0530

    client side updated

--- PGclient/src/App.jsx
+++ PGclient/src/App.jsx
@@ -1,11 +1,53 @@
-import { useState } from 'react'
+import axios from "axios";
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [price, setPrice] = useState(500);
+
+  const initPayment = (data) => {
+    const options = {
+      key: "rzp_test_97dwCasNnK2z0t",
+      amount: data.amount,
+      currency: data.currency,
+      name: "book.name",
+      description: "test transaction",
+      order_id: data.id,
+      handler: async (response) => {
+        try {
+          const verifyUrl = "http://localhost:8080/api/payment/verify";
+          const { data } = await axios.post(verifyUrl, response);
+          console.log(data);
+        } catch (error) {
+          console.log(error);
+        }
+      },
+      theme: {
+        color: "#3399cc",
+      }
+    };
+    const rzp1 = new window.Razorpay(options);
+    rzp1.open();
+  };
+
+  const handlePayment = async () => {
+    try {
+      const orderUrl = "http://localhost:8080/api/payment/orders";
+      const { data } = await axios.post(orderUrl, { amount: price });
+      console.log(data);
+      initPayment(data.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className="App">
-      Hi Samyak!
+      <button onClick={handlePayment}>
+        buy now
+      </button>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
