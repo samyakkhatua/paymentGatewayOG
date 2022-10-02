616e62aa127b33780b22c1a1364cf90fa2ef7114
samyakkhatua <samyakkhatua@duck.com>
2022-10-02 23:40:02 +0530

    server update

--- PGserver/Routes/payment.js
+++ PGserver/Routes/payment.js
@@ -18,43 +18,79 @@ router.post("/orders", async (req, res) => {
 
     //API request and response for creating an order
     instance.orders.create(options, (error, order) => {
-
       if (error) {
         console.log(error);
         return res.status(500).json({ message: "Something went Wrong!" });
       }
 
-      console.log({ data: order });
+      // console.log({ data: order });
       res.status(200).json({ data: order });
     });
   } catch (error) {
-    res.status(500).json({ message: "Internal Server Error!" });
+    res.status(500).json({ message: "creating Order Internal Server Error!" });
     console.log(error);
   }
 });
 
 //verify payment
-router.post("/verify", async (req, res) => {
-  try {
-    const { razorpay_order_id, razorpay_payment_id, razorpay_signature } =
-      req.body;
+// router.post("/verify", async (req, res) => {
+//   try {
+//     const { razorpay_order_id, razorpay_payment_id, razorpay_signature } =
+//       req.body;
 
-    const sign = razorpay_order_id + "|" + razorpay_payment_id;
+//     const sign = razorpay_order_id + "|" + razorpay_payment_id;
 
-    const expectedSign = crypto
-      .createHmac("sha256", process.env.KEY_SECRET)
-      .update(sign.toString())
-      .digest("hex");
+//     const expectedSign = crypto
+//       .createHmac("sha256", process.env.KEY_SECRET)
+//       .update(sign.toString())
+//       .digest("hex");
 
-    if (razorpay_signature === expectedSign) {
-      return res.status(200).json({ message: "Payment verified successfully" });
-    } else {
-      return res.status(400).json({ message: "Invalid signature sent!" });
-    }
-  } catch (error) {
-    res.status(500).json({ message: "Internal Server Error!" });
-    console.log(error);
-  }
-});
+//     if (razorpay_signature === expectedSign) {
+//       // res.redirect(
+//       //   `http://localhost:5173/thank-you?reference=${razorpay_payment_id}`
+//       //   // `http://localhost:5173/`
+//       // );
+
+//       return res.status(200).json({ message: "Payment verified successfully" });
+//       // return;
+
+
+//     } 
+//     else {
+//       return res.status(400).json({ message: "Verify: Invalid signature sent!!!" });
+//     }
+
+//   } catch (error) {
+
+//     console.log(error);
+//     res.status(500).json({ message: "verify: Internal Server Error!" });
+
+//   }
+//   return;
+// });
+
+// //verify webhook
+// router.post("verify/razorpay-signature", (req, res) => {
+//   console.log(JSON.stringify(req.body));
+
+//   // const sign = razorpay_order_id + "|" + razorpay_payment_id;
+
+//   //hash
+//   const expectedSign = crypto
+//       .createHmac("sha256", "samyakkhatua")
+//       .update(JSON.stringify(req.body))
+//       .digest("hex");
+    
+//   console.log(expectedSign);
+//   console.log(req.headers["x-razorpay-signature"]);
+
+//     // if (expectedSign === req.headers["x-razorpay-signature"]) {
+      
+//     // } else {
+      
+//     // }
+  
+//   res.status(200);
+// })
 
 module.exports = router;
--- PGserver/index.js
+++ PGserver/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const paymentRoutes = require("./Routes/payment")
+const crypto = require("crypto");
 
 //initialize app
 const app = express();
@@ -15,6 +16,28 @@ app.use(cors());
 
 //routes
 app.use("/api/payment/", paymentRoutes);
+
+//verify webhook
+app.post("/verify/razorpay-signature", (req, res) => {
+    console.log(JSON.stringify(req.body));
+
+    //hash
+    const expectedSign = crypto
+        .createHmac("sha256", "samyakkhatua")
+        .update(JSON.stringify(req.body))
+        .digest("hex");
+      
+    console.log(expectedSign);
+    console.log(req.headers["x-razorpay-signature"]);
+  
+      if (expectedSign === req.headers["x-razorpay-signature"]) {
+        return res.status(200).json({ message: " inside if: Payment verified successfully" });
+      } else {
+        
+      }
+
+      res.status(200).json({ message: "Payment verified successfully" });
+    })
 
 //app listening
 const port = process.env.PORT || 8080;
